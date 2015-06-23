0c0f270b407df9d28bd4a9e1f471711ec54676ee
Swarup Karavadi <swarup.karavadi@gmail.com>
2015-06-23 22:20:35 +0530

    Added bash resource to nodejs default recipe

--- chef-repo/cookbooks/nodejs/recipes/default.rb
+++ chef-repo/cookbooks/nodejs/recipes/default.rb
@@ -6,3 +6,12 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# bash resource to install nodejs
+bash "nodejs" do
+	code <<-EOH
+		wget http://nodejs.org/dist/v0.12.5/node-v0.12.5-linux-x64.tar.gz
+		sudo tar -C /usr/local --strip-components 1 -xzf node-v0.12.5-linux-x64.tar.gz 
+		rm node-v0.12.5-linux-x64.tar.gz 
+	EOH
+end
